4d40f5051defff488e9299a92e6f6f8640bb67e0
pankaj-kb <pankajb2601@gmail.com>
2023-02-02 23:09:33 +0530

    completed Li task

---
+++ script.js
@@ -0,0 +1,12 @@
+let navItems = document.querySelector("header nav ul");
+
+let newNavLi = document.createElement("li");
+
+
+let newA = document.createElement("a");
+newA.href = "./hire-me.html";
+newA.innerText = "Hire Me";
+
+newNavLi.appendChild(newA);
+
+navItems.appendChild(newNavLi);
