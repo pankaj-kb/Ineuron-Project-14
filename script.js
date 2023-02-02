6392f1e7f7e0ca506aa7dfe011184d5d7970b028
pankaj-kb <pankajb2601@gmail.com>
2023-02-03 00:47:33 +0530

    completed till new functionality task 3

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Task 2 placeholder text
-
-let inputField = document.querySelector(".search-field input");
-
-inputField.placeholder = "Search My Projects";
-
-
-let spanText = document.getElementsByTagName("span");
-
-for (let i = 0; i < spanText.length; i++) {
-    if (spanText[i].innerText == "a Freelancer") {
-        console.log("yes it works");
-        spanText[i].innerText = "an Employee"
-    }
-    if (spanText[i].innerText == "National and International Client") {
-        console.log("this also works");
-        spanText[i].innerText = "Ineuron Intelligence Private Ltd"
-    }
-}
-
 // Task 4 img
 
 let myImg = document.querySelector(".hero-right-section img");
@@ -41,30 +21,51 @@ let count = "";
 let trackChange = document.querySelector(".progress button");
 
 trackChange.addEventListener("click", () => {
-    // Task 3 InnerText
     count++;
-    console.log(count);
-    if (count == 1) {
-
-        //Task 1 Adding li ("Hire Me")
-
+    if (count <= 5) {
         trackChange.innerText = "View Task " + count + " Changes";
+        console.log(count);
 
-        let navItems = document.querySelector("header nav ul");
+        if (count == 1) {
 
-        let newNavLi = document.createElement("li");
+            //Task 1 Adding li ("Hire Me")
+            let navItems = document.querySelector("header nav ul");
+
+            let newNavLi = document.createElement("li");
 
 
-        let newA = document.createElement("a");
-        newA.href = "./hire-me.html";
-        newA.innerText = "Hire Me";
+            let newA = document.createElement("a");
+            newA.href = "./hire-me.html";
+            newA.innerText = "Hire Me";
 
-        newNavLi.appendChild(newA);
+            newNavLi.appendChild(newA);
 
-        navItems.appendChild(newNavLi);
-    }
+            navItems.appendChild(newNavLi);
+        }
 
-    if (count == 2) {
+        if (count == 2) {
+            // Task 2 placeholder text
 
+            let inputField = document.querySelector(".search-field input");
+
+            inputField.placeholder = "Search My Projects";
+        }
+
+        if (count == 3) {
+
+            // Task 3 InnerText
+            let spanText = document.getElementsByTagName("span");
+
+            for (let i = 0; i < spanText.length; i++) {
+                if (spanText[i].innerText == "a Freelancer") {
+                    console.log("yes it works");
+                    spanText[i].innerText = "an Employee"
+                }
+                if (spanText[i].innerText == "National and International Client") {
+                    console.log("this also works");
+                    spanText[i].innerText = "Ineuron Intelligence Private Ltd"
+                }
+            }
+        }
     }
 })
